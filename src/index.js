a2b87b528383810b2e343c3d449d376b2bfd560e
Sally Diament <sally.barzilai@gmail.com>
2022-10-04 15:14:02 -0300

    incluindo o requisito 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const path = require('path');
+
+const pathTalker = path.resolve(__dirname, 'talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,3 +19,26 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker', async (req, res) => {
+  const talkers = JSON.parse(await fs.readFile(pathTalker, 'utf8'));
+  res.status(HTTP_OK_STATUS).json(talkers);
+})
+
+// app.get('/talker/:id', async (req, res) => {
+//   const { id } = req.params;
+//   const { name, age, talk } = req.body;
+//   let selectedTalker;
+
+//   for (let i = 0; i < teams.length; i += 1) {
+//     const team = teams[i];
+
+//     if (team.id === Number(id)) {
+//       team.name = name;
+//       team.initials = initials;
+//       updatedTeam = team;
+//     }
+//   }
+
+//   res.status(200).json({ updatedTeam });
+// })
